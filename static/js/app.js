23db273e1fd7c29cc074e7edd03e7014a2eef6b1
kerryharp <kharp@umn.edu>
2021-04-14 21:38:57 -0500

    preparing for initial graphing

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,35 @@
 // Use D3 fetch to read the JSON file and occupy the sample IDs
-// Get data by using data binding. See class lecture on D3-1, examples 1 & 2
+// Get data by using data binding. See class lecture on D3-1, example 3
 function init() {
-     // Populate dropdown options with all the individual ID's of the study subjects.
      d3.json("data/samples.json").then(function(data) {
-         d3.select("#selDataset").selectAll("option")
+         // what to bind the data to
+         d3.select("#selDataset")
+            // append to the code all that follows
+             .selectAll("option")
+             // mapping the array
              .data(data.names)
              .enter()
+             // updating html
              .append("option")
+             // function of data bound to wanted elements
              .html(function(d) {
                   return `<option>${d}</option>`;
              });
      });
- }
+ };
 
-
- // event handler
-
- //  build plot
+// plot the initial/default
 
 
 
+// event handler for changing the selection in dropdown
 
- init();
+
+
+//  build plots
+
+
+
+// calling initial function
+init();
 
