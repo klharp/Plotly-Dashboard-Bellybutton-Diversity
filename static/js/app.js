22c4d0665f5fe1247ca0efc7dce17ece13999d74
kerryharp <kharp@umn.edu>
2021-04-18 18:11:05 -0500

    reading json and populating dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -1,115 +1,28 @@
-//  **Import of json data**  //
-// Use D3 fetch to read the JSON file and occupy the sample IDs dropdown
-// Get data for the dropdown options by using data binding. See class lecture on D3-1, example 3
-d3.json("data/samples.json").then(function(importdata) {
+// Portions of this code are taken directly from instructor's office hours tutorial 
+// Permission was given to use.
+// I had previously successfully imported the json and occupied the dropdown.
+// I decided to redo the code based on the office hours tutorial.
+// That initial code exists as previous commits in Github.
 
-     // Define a variable for the imported data
-     alldata = importdata;
+ //  **Use D3 fetch to read the JSON file and occupy the sample IDs dropdown**  //
+ function init() { 
 
-     console.log(alldata);
+     // Populate the dropdown
+     var selector = d3.select("#selDataset");
 
-     // what to bind the data to
-     d3.select("#selDataset")
-          // append to the code all that follows
-          .selectAll("option")
-          // mapping the array
-          .data(importdata.names)
-          .enter()
-          // updating html
-          .append("option")
-           // function of data bound to wanted elements
-          .html(function(d) {
-                return `<option>${d}</option>`;
-          })
+     d3.json("data/samples.json").then(data => {
+          console.log(data);
 
+          var sampleNames = data.names;
+          console.log(sampleNames);
 
-     //  **Initialize the defaults**  //
-     function init() { 
+          sampleNames.forEach(sampleID => {
+               selector.append("option")
+                    .text(sampleID)
+                    .property("value", sampleID);
+          });
+     });
 
-          // Get the all the data for id 940 (first in the list) and assign to a variable
-           var initialData = alldata.samples[0];
+}
 
-          // console.log(initialData);
-
-          // Get the data from the above variable for the initial sample
-          var initialValues = initialData.sample_values;
-          var initialOtuIds = initialData.otu_ids;
-          var initialOtuLabels = initialData.otu_labels;
-
-          // console.log(initialValues);
-          // console.log(initialOtuIds);
-          // console.log(initialOtuLabels);
-
-          // Get the top 10 OTUs from in inital data
-          // See class exercise 05, Interactive Plotly I on slicing to slice the data for the top 10
-          // Reverse the order so it plots correctly on horizontal bar chart
-          var tenInitialValues = initialValues.slice(0,10).reverse();
-          var tenInitialOtuIds = initialOtuIds.slice(0,10).reverse();
-          var tenInitialOtuLabels = initialOtuLabels.slice(0,10).reverse();
-
-          console.log(tenInitialValues);
-          console.log(tenInitialOtuIds);
-          console.log(tenInitialOtuLabels);
-
-          //  **Build plot**  //
-          // Bar plot
-          // <div id="bar">
-          // trace1
-          var trace1 = {
-               x: tenInitialValues,
-               y: tenInitialOtuIds.map(otuId => `OTU ${otuId}`),
-               text: tenInitialOtuLabels,
-               type: "bar",
-               orientation: "h"
-          };
-
-          // Trace data in an array
-          var data = [trace1];
-
-          // Define layout
-          var layout = {
-               title: "Top 10 OTUs",
-               xaxis: {title: "Sample Value"},
-               yaxis: {title: "OTU ID"}
-          };
-
-          // Call the plot
-          Plotly.newPlot("bar", data, layout);
-
-     }
-     
-     init();
-
-     
-});
-
-
-
-
-// //  **Define event handler for changing the selection in dropdown**  //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//      });
-// };
-
-// 
-
-// <div id="gauge">
-// <div id="bubble">
-
-
-// Calling initial function
+ init();
