2473ce3afdc962be5b289419ef078c6fd2be26c8
kerryharp <kharp@umn.edu>
2021-04-18 22:49:24 -0500

    gauge working for default only

--- static/js/app.js
+++ static/js/app.js
@@ -27,7 +27,7 @@ function drawBarGraph(sampleId) {
           //console.log(otu_labels);
           //console.log(sample_values);
 
-          yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId}`).reverse();
+          yticks = otu_ids.slice(0,10).map(otuId => `OTU ${otuId} `).reverse();
 
           var barData = {
                x: sample_values.slice(0,10).reverse(),
@@ -35,7 +35,7 @@ function drawBarGraph(sampleId) {
                type: "bar",
                text: otu_labels.slice(0,10).reverse(),
                orientation: "h",
-               marker: {color: "red"}
+               marker: {color: "#ff4d4d"} //{color: "#ff94c9"}
           }
 
           var barArray = [barData];
@@ -92,14 +92,14 @@ function drawBubbleChart(sampleId) {
      
           // Div id=bubble
           Plotly.newPlot("bubble", bubbleArray, bubbleLayout)
-     });
-     
+     });    
 }
+
 
 // Function for Metadata, extract key:value pairs
 // Class lecture Javascript II, Object iteration (07).
 function showMetadata(sampleId) {
-     console.log(`showMetadata(${sampleId})`);
+     //console.log(`showMetadata(${sampleId})`);
 
      //Reading data each time drawing a graph
      d3.json("data/samples.json").then(data => {
@@ -111,22 +111,69 @@ function showMetadata(sampleId) {
           var result = resultArray[0];
           //console.log(result);
 
-          var metaPanel = d3.select("#sample-metadata");
+          // https://stackoverflow.com/questions/26629180/changing-the-case-of-javascript-object-keysvar 
+          metaPanel = d3.select("#sample-metadata");
           metaPanel.html("");
           Object.entries(result).forEach(([key, value]) => {
-               metaPanel.append("h6").text(`${key}: ${value}`)
+               metaPanel.append("h6").text(`${key.toUpperCase()}: ${value}`)
           });
      });
 }
 
+
 // Function for Gauge Gauge
+//https://plotly.com/javascript/gauge-charts/
 function drawGauge(sampleId) {
-     console.log(`drawGauge(${sampleId})`);
+     //console.log(`drawGauge(${sampleId})`);
+
+     //Reading data each time drawing a graph
+     d3.json("data/samples.json").then(data => {
+          //console.log(data);
+          
+          var samples = data.metadata;
+          // Filter samples where sample id = desired sample id
+          var resultArray = samples.filter(s => s.id == sampleId);
+          var result = resultArray[0];
+          //console.log(result);
+
+          var gaugedata = [ {
+               //domain: { x:[0,1], y:[0,1] },
+               type: "indicator",
+               mode: "gauge+number",
+               value: result.wfreq,
+               title: { text: "Belly Button Washing Frequency <br> Scrubs Per Week"},
+               gauge: {
+                    axis: { visible: true, range: [null, 9], tickmode: "linear", nticks:10},
+                    bar: {'color': "black"},
+                    steps: [
+                         { range: [0, 1], color: "rgb(15,42,251)" },
+                         { range: [1, 2], color: "rgb(43,114,251)" },
+                         { range: [2, 3], color: "rgb(109,205,253)" },
+                         { range: [3, 4], color: "rgb(203,205,254)" },
+                         { range: [4, 5], color: "rgb(245,245,255)" },
+                         { range: [5, 6], color: "rgb(254,189,254)" },
+                         { range: [6, 7], color: "rgb(253,106,184)" },
+                         { range: [7, 8], color: "rgb(253,104,117)" },
+                         { range: [8, 9], color: "rgb(252,18,31)" },
+                    ]
+               }
+           }];
+        
+     var gaugelayout = {
+     width: 450,
+     height: 500,
+     margin: { t: 0, l: 0, b: 0, r:0},
+     };
+        
+     Plotly.newPlot("gauge", gaugedata, gaugelayout);
+
+     });       
 }
+
 
 // Establish event handler
 function optionChanged(newsampleId){
-     console.log(`User selected ${newsampleId}`);
+     //console.log(`User selected ${newsampleId}`);
 
      drawBarGraph(newsampleId);
      drawBubbleChart(newsampleId);
@@ -163,8 +210,6 @@ function optionChanged(newsampleId){
           drawGauge(id);
          
      });
-
-
 }
 
  init();
